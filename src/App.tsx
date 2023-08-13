8c14ab0084f3737dfdd73a3b4bee257f582ddceb
nangkyeonglim <nangkyeonglim@gmail.com>
2023-08-14 06:44:36 +0900

    feat: Layout 구현
    Co-authored-by: Jeremy <shackstack@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import WriteNoticePage from './pages/WriteNoticePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <WriteNoticePage />
+    </Layout>
   );
 }
 
 export default App;
+
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 100vw;
+  height: 100vh;
+`;
