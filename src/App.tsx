4786f229bce35ad492da38897299d74d3d85af42
nangkyeonglim <nangkyeonglim@gmail.com>
2023-08-15 19:10:33 +0900

    chore: react-router-dom 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import styled from 'styled-components';
-import WriteNoticePage from './pages/WriteNoticePage';
+import { Outlet } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <Layout>
-      <WriteNoticePage />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Outlet />
+      </Layout>
+    </QueryClientProvider>
   );
 }
 
