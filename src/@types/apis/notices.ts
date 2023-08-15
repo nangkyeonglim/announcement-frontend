4b0e3ccb499ee8e19eea10dc6c74e61a69a46484
nangkyeonglim <nangkyeonglim@gmail.com>
2023-08-15 19:10:55 +0900

    feat: `getNotice` API 구현

---
+++ src/@types/apis/notices.ts
@@ -0,0 +1,9 @@
+export type GetNoticeResponse = {
+  id: number;
+  title: string;
+  content: string;
+  channelName: string;
+  writer: string;
+  createdAt: string;
+  updatedAt: string;
+};
--- src/apis/notices.ts
+++ src/apis/notices.ts
@@ -1,8 +1,17 @@
 import { apiClient } from '.';
+import { GetNoticeResponse } from '../@types/apis/notices';
 import type { Notice } from '../@types/domain';
 
 export const addNotice = async (newNotice: Notice) => {
   const response = await apiClient.post<void>('/notices', newNotice);
+
+  return response.data;
+};
+
+export const getNotice = async (noticeId: number) => {
+  const response = await apiClient.get<GetNoticeResponse>(
+    `/notices/${noticeId}`
+  );
 
   return response.data;
 };
